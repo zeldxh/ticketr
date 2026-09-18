ec0cea439d76cb10d009482002a51c041534d592
zeldxh <783613+zeldxh@users.noreply.github.com>
2026-09-18 00:05:21 -0600

    feat(domain): add usuario entity

---
+++ src/main/java/com/example/sistemaboletos/model/Usuario.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.sistemaboletos.model;
+
+/**
+ *
+ * @author straker
+ */
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario extends EntidadBase {
+    private String nombre;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+    
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Compra> compras = new ArrayList<>();
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+}
