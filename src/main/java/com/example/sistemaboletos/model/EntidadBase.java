5ecdb4a45aabfc447c0a9c45cef5112dd50ff45a
zeldxh <783613+zeldxh@users.noreply.github.com>
2026-09-18 00:05:21 -0600

    feat(domain): add entidadbase abstract entity and rol enum

---
+++ src/main/java/com/example/sistemaboletos/model/EntidadBase.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.sistemaboletos.model;
+
+/**
+ *
+ * @author straker
+ */
+
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class EntidadBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/example/sistemaboletos/model/Rol.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.sistemaboletos.model;
+
+/**
+ *
+ * @author straker
+ */
+
+public enum Rol {
+    USUARIO,
+    ADMIN
+}
