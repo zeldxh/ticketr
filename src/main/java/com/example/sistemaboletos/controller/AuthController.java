acb42fc220dc3388aae65efc1af5633fa09549a9
zeldxh <783613+zeldxh@users.noreply.github.com>
2026-09-20 21:23:38 -0600

    feat(controller): add authcontroller for login and registration

---
+++ src/main/java/com/example/sistemaboletos/controller/AuthController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.sistemaboletos.controller;
+
+/**
+ *
+ * @author straker
+ */
+
+import com.example.sistemaboletos.model.Usuario;
+import com.example.sistemaboletos.model.servicio.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public AuthController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping("/login")
+    public String mostrarLogin() {
+        return "login";
+    }
+
+    @GetMapping("/registro")
+    public String mostrarRegistro(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "registro";
+    }
+
+    @PostMapping("/registro")
+    public String registrar(@ModelAttribute Usuario usuario) {
+        usuarioService.registrar(usuario);
+        return "redirect:/login?registro_exitoso";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "access-denied";
+    }
+}
