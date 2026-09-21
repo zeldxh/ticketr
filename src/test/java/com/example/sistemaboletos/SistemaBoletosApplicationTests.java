4c9f786f6984bbd115b16dd566faded9756dcb59
zeldxh <783613+zeldxh@users.noreply.github.com>
2026-09-20 21:24:02 -0600

    test(app): add context-load smoke test

---
+++ src/test/java/com/example/sistemaboletos/SistemaBoletosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemaboletos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBoletosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
