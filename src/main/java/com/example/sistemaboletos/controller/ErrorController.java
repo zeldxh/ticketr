90e15dcf9b33484f668b59f7703a429e13b31f79
zeldxh <783613+zeldxh@users.noreply.github.com>
2026-09-20 21:23:38 -0600

    feat(controller): add errorcontroller for custom error handling

---
+++ src/main/java/com/example/sistemaboletos/controller/ErrorController.java
@@ -0,0 +1,13 @@
+package com.example.sistemaboletos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+    
+    @GetMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+} 
